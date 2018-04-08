e4c429d33972069d9f1b28cd40a9b812a939b0b7
Peter Gerber <peter@arbitrary.ch>
2018-04-08 14:12:00 +0200

    Only show usage info when appropriate

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,15 @@ use std::env;
 
 const DEFAULT_PROGNAME: &str = "grpexec";
 
-fn abort(msg: &str) -> ! {
+fn abort(msg: &str, show_usage: bool) -> ! {
     let program_name = env::args_os().next().map_or_else(
         || DEFAULT_PROGNAME.to_string(),
         |n| n.to_string_lossy().to_string(),
     );
     eprintln!("ERROR: {}", msg);
-    eprintln!("Usage: {} GROUP COMMAND [ARG]...", program_name);
+    if show_usage {
+        eprintln!("Usage: {} GROUP COMMAND [ARG]...", program_name);
+    }
     process::exit(127);
 }
 
@@ -24,19 +26,19 @@ fn main() {
     let mut args = env::args_os();
     let group = match args.by_ref().nth(1) {
         Some(c) => c.into_string().expect("group name is not UTF-8 encoded"),
-        None => abort("A group must be specified"),
+        None => abort("A group must be specified", true),
     };
     let cmd = match args.by_ref().next() {
         Some(c) => c,
-        None => abort("A command must be specified"),
+        None => abort("A command must be specified", true),
     };
 
     // set group and drop privileges
     if let Err(e) = grpexec::drop_privileges_with_group(&group) {
-        abort(&format!("Failed to change group to {:?}: {}", &group, e));
+        abort(&format!("Failed to change group to {:?}: {}", &group, e), false);
     }
 
     // execute command
     let err = Command::new(&cmd).args(args).exec();
-    abort(&format!("Failed to execute command {:?}: {}", cmd, err));
+    abort(&format!("Failed to execute command {:?}: {}", cmd, err), false);
 }
